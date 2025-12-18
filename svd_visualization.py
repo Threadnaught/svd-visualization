149d72d26e23b45f92eff2de1e98fe3145a0f6a2
idk <patrickhingley@live.co.uk>
2025-12-18 17:45:49 +0000

    vis v2

--- svd_visualization.py
+++ svd_visualization.py
@@ -6,44 +6,47 @@ from coords_and_svd import n, coords_world, S, Vh
 
 # Play with these booleans:
 scale_basis_by_significance = True
-
 show_zeroth_basis = True
 show_first_basis_possiblities = False
 show_first_basis = True
 show_second_basis = True
 
-# We're handling the bases collectively, bools above are mostly for convinience of experimenter
+# We're handling the bases collectively, bools above are mostly for convinience of experimentation
 show_basis = [show_zeroth_basis, show_first_basis, show_second_basis]
-
-rot_amount = 0.025
-
-# Generate random rotation as per https://stackoverflow.com/a/44031492
-u,v,w = np.random.uniform(0,1,size=3)
-full_rotation_quat = np.quaternion(np.sqrt(1-u) * np.sin(2 * np.pi * v), np.sqrt(1-u) * np.cos(2 * np.pi * v), np.sqrt(u) * np.sin(2 * np.pi * w), np.sqrt(u) * np.cos(2 * np.pi * w)) 
-rotation_quat = quaternion.slerp(np.quaternion(1,0,0,0), full_rotation_quat, 0, 1, rot_amount)
 
 #viewport rotation:
 viewport_quat = quaternion.from_euler_angles([0,np.pi/4,np.pi/4])
 
-#basis vecs:
+# Each frame, we rotate the scene by a set amount. We want it to line up with the frame count to loop perfectly
+animation_frames = 80
+rotation_axis = np.asarray([0.5,1,0.25])
+rotation_step_rads = 2*np.pi/animation_frames
+
+# Quaternion magic:
+rotation_step_imag = (rotation_axis / np.linalg.norm(rotation_axis)) * np.sin(rotation_step_rads/2)
+rotation_step_quat = np.quaternion(np.cos(rotation_step_rads/2), rotation_step_imag[0], rotation_step_imag[1], rotation_step_imag[2])
+
+#Basis vecs:
 if scale_basis_by_significance:
     S /= 5 # To fit better
 else:
     S = np.asarray([100, 100, 100])
 
+# These are our basis vecs. Because we've already rotated the vector space to align with SVD, theyre the x, y and z unit vectors
 basis_vecs = np.diag(S)
+
+# These are the possiblities for the second basis vec, theyre all of the vecs that are perpundicular to the first
 first_basis_possibilities = np.asarray([[0, np.sin(th), np.cos(th)] for th in np.arange(0, 2*np.pi, 0.1*np.pi)]) * 50
-
-
-lim = np.max(np.abs(coords_world)) * 1.1
 
 #create plot:
 fig = plt.figure()
 ax = fig.add_subplot()
 ax.axis('off')
 
-#plot pointss (zero because draw logic is in update)
+#plot points (zero because draw logic is in update)
 scatter = ax.scatter(np.zeros(n), np.zeros(n), color='black')
+
+lim = np.max(np.abs(coords_world)) * 1.1
 ax.set_xlim(-lim, lim)
 ax.set_ylim(-lim, lim)
 
@@ -60,15 +63,15 @@ if show_first_basis_possiblities:
     
 def update(frame):
     global coords_world, basis_vecs, first_basis_possibilities
-    coords_world = quaternion.rotate_vectors(rotation_quat, coords_world)
+    coords_world = quaternion.rotate_vectors(rotation_step_quat, coords_world)
     coords_viewport = quaternion.rotate_vectors(viewport_quat, coords_world)
     
     updates = [scatter]
 
-    basis_vecs = quaternion.rotate_vectors(rotation_quat, basis_vecs)
+    basis_vecs = quaternion.rotate_vectors(rotation_step_quat, basis_vecs)
     basis_vecs_viewport = quaternion.rotate_vectors(viewport_quat, basis_vecs)
 
-    first_basis_possibilities = quaternion.rotate_vectors(rotation_quat, first_basis_possibilities)
+    first_basis_possibilities = quaternion.rotate_vectors(rotation_step_quat, first_basis_possibilities)
     first_basis_possibilities_viewport = quaternion.rotate_vectors(viewport_quat, first_basis_possibilities)
 
     for i in range(3):
@@ -85,11 +88,9 @@ def update(frame):
     
     return updates
 
-ani = animation.FuncAnimation(fig, func=update, interval=50, frames=1, cache_frame_data=False)
+ani = animation.FuncAnimation(fig, func=update, interval=50, frames=animation_frames, cache_frame_data=False)
 
-#writer = animation.PillowWriter(fps=10,
-#                                metadata=dict(artist='https://github.com/Threadnaught'),
-#                                bitrate=1800)
-#ani.save('gifs/s5.gif', writer=writer)
+writer = animation.PillowWriter(fps=20, metadata=dict(artist='https://github.com/Threadnaught'), bitrate=1800)
+ani.save('gifs/second-basis-applied.gif', writer=writer)
 
 plt.show()
